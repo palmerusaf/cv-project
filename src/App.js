a9fb56f1b00816609590075c167708daaf7c4e61
Branden Palmer <brandenpalmer08@gmail.com>
2022-05-31 10:56:19 +0700

    add education and work experience sections

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import Title from "./components/title.js";
 import Directions from "./components/directions.js";
 import CategoryHeading from "./components/category-heading.js";
-import ContactSection from "./components/contact-section.js";
+import PublishableForm from "./components/publishable-form";
 
 class App extends Component {
   constructor(props) {
@@ -15,9 +15,31 @@ class App extends Component {
         <Title />
         <Directions />
         <CategoryHeading content="Contact" />
-        <ContactSection></ContactSection>
+        <PublishableForm
+          formInputs={[
+            { type: "text", label: "name" },
+            { type: "email", label: "email" },
+            { type: "tel", label: "phoneNumber" },
+          ]}
+        />
         <CategoryHeading content="Education" />
+        <PublishableForm
+          formInputs={[
+            { type: "text", label: "schoolName" },
+            { type: "text", label: "titleOfStudy" },
+            { type: "date", label: "DateOfStudy" },
+          ]}
+        />
         <CategoryHeading content="Work Experience" />
+        <PublishableForm
+          formInputs={[
+            { type: "text", label: "companyName" },
+            { type: "text", label: "position" },
+            { type: "text", label: "tasksPerformed" },
+            { type: "date", label: "dateFrom" },
+            { type: "date", label: "dateTo" },
+          ]}
+        />
       </div>
     );
   }
